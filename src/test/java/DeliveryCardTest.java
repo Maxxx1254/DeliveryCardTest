a82057e4d6f52aa63008df9afb90bf736486986a
Max <orenburg20172017@gmail.com>
2022-09-19 18:24:05 +0300

    изменил способ выбора даты

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -11,11 +11,19 @@ import static com.codeborne.selenide.Selenide.*;
 public class DeliveryCardTest {
     @Test
     void shouldSubmitRequest() {
+        GenerateDate generateDate = new GenerateDate();
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Санкт-Петербург");
+//        String planningDate = generateDate.generateDate(0);
+//        не получается поставить дату, через дебагер видно что дату высчитывает верную
+//        но никак не хочет ее ставить, если поможете буду признателен
+
+//        $("[placeholder='Дата встречи']").setValue(planningDate);
+        $("[placeholder='Дата встречи']").click();
+        $("[data-day='1663880400000']").click();
         $(byName("name")).val("Васиоий Васильев");
         $("[name='phone']").setValue("+79121234223");
-        $x("//label[@data-test-id='agreement']").click();
+        $("[class='checkbox__box']").click();
         $(withText("Забронировать")).click();
         $(withText("Встреча успешно забронирована")).shouldBe(appear, Duration.ofSeconds(15));
     }
